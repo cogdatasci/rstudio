39ac99091f2cf5a0c9aab5b7a6219ed59f26b955
cogdatasci <77553463+cogdatasci@users.noreply.github.com>
2021-04-14 14:05:14 -0700

    Update set_theme.sh

--- scripts/set_theme.sh
+++ scripts/set_theme.sh
@@ -3,8 +3,5 @@
 THEME=${THEME:=none}
 
 if [ "$THEME" != none ]; then
-	mkdir -p /home/rstudio/.rstudio/monitored/user-settings
-	echo "uiPrefs={\"theme\" : \"$THEME\"}" > \
-	/home/rstudio/.rstudio/monitored/user-settings/user-settings
-	chown -R rstudio /home/rstudio
+	echo "{ "editor_theme": \"$THEME\" }" > ~/.config/rstudio/rstudio-prefs.json
 fi
